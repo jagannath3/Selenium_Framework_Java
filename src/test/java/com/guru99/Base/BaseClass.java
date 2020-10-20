85e8f9833f1c378e1fd0bfb0177bb6eb5a654cd0
jagannath3 <kljagannath3@gmail.com>
2020-10-20 19:09:15 +0530

    Day 2 commit 3 Log4j integration

--- src/test/java/com/guru99/Base/BaseClass.java
+++ src/test/java/com/guru99/Base/BaseClass.java
@@ -2,6 +2,8 @@ package com.guru99.Base;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,10 +11,15 @@ import com.guru99.resources.DataProviders;
 
 public class BaseClass extends DataProviders {
 
+	public final static Logger log=LogManager.getLogger(BaseClass.class);
+	
 	public WebDriver initialization() {
 		System.setProperty("webdriver.chrome.driver", Chrome_Path);
 		WebDriver driver = new ChromeDriver();
+		log.info("***********************Starting Tests***********************");
+		log.info("Driver initialized");
 		driver.get(URL);
+		log.info("Redirected to -> "+URL);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
--- src/test/java/com/guru99/testCases/tc_01_LoginTest.java
+++ src/test/java/com/guru99/testCases/tc_01_LoginTest.java
@@ -7,6 +7,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
 import com.guru99.Base.BaseClass;
 import com.guru99.pageObjects.LoginPage;
 import com.guru99.resources.DataProviders;
@@ -33,42 +34,56 @@ public class tc_01_LoginTest extends BaseClass {
 
 	@BeforeTest
 	public void initDriver() {
-
 		driver = initialization();
 	}
 
 	@Test(dataProvider = "validLoginData", dataProviderClass = DataProviders.class)
 	public void validLoginTest(String username, String password) throws NoAlertPresentException {
+		log.info("***********************Running validLoginTest***********************");
 		lp = new LoginPage(driver);
 		cu = new ClickUtility(driver);
 		lp.getLoginUserName().sendKeys(username);
+		log.debug("user name entered");
 		lp.getLoginPassword().sendKeys(password);
+		log.debug("password entered");
 		cu.clickLogin();
+		log.debug("Clicked on Login");
 		String currentUrl = driver.getCurrentUrl();
 		Assert.assertTrue(currentUrl.contains("Managerhomepage"), "Login Successfull");
-		System.out.println("Login Successfull! You're redirected to " + currentUrl);
+		log.info("Login Successfull! You're redirected to " + currentUrl);
 		cu.clickLogout();
+		log.debug("Clicked on Logout");
 		String logOutMsg = driver.switchTo().alert().getText();
 		Assert.assertTrue(ExpectedConditions.alertIsPresent() != null, logOutMsg);
+		log.info("Popup displayed with message: " + logOutMsg);
 		driver.switchTo().alert().accept();
-		System.out.println(logOutMsg);
+		log.info("Clicked OK on Popup and it is closed");
+		log.info("***********************End of validLoginTest***********************");
 	}
 
 	@Test(dataProvider = "invalidLoginData", dataProviderClass = DataProviders.class)
 	public void invalidLoginTest(String username, String password) {
+		log.info("***********************Running invalidLoginTest***********************");
 		lp = new LoginPage(driver);
 		cu = new ClickUtility(driver);
 		lp.getLoginUserName().sendKeys(username);
+		log.debug("user name entered");
 		lp.getLoginPassword().sendKeys(password);
+		log.debug("password entered");
 		cu.clickLogin();
+		log.debug("Clicked on Login");
 		String loginErrorMsg = driver.switchTo().alert().getText();
 		Assert.assertTrue(ExpectedConditions.alertIsPresent() != null, loginErrorMsg);
-		System.out.println(loginErrorMsg);
+		log.error("Popup displayed with error message: " + loginErrorMsg);
 		driver.switchTo().alert().accept();
+		log.info("Clicked OK on Popup and it is closed");
+		log.info("***********************End of invalidLoginTest***********************");
 	}
 
 	@AfterTest
 	public void tearDown() {
 		driver.close();
+		log.info("Browser closed");
+		log.info("***********************End of Tests***********************");
 	}
 }
